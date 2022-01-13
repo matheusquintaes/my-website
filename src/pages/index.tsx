7706d255b9f2a470211b8bc279c590dc9d09c074
Matt Castro <mattcastro@Matts-MacBook-Air.local>
2022-01-14 11:47:49 +1300

    boilerplate 1.0

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+type Props = {
+  title: string
+}
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -13,7 +17,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js na pratica!</a>
         </h1>
 
         <p className={styles.description}>
