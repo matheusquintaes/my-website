ddfa957713e1db30aef15645dafafdbcc97b1e23
Matt Quintaes <matheus.quintaes@gmail.com>
2022-11-22 15:22:21 +1300

    Update carrousel

--- src/components/Carroussel/index.jsx
+++ src/components/Carroussel/index.jsx
@@ -6,9 +6,9 @@ const Carroussel = () => (
       <S.Gradient />
       <S.Items>
         <S.Item>
-          HTML, CSS, SASS, Javascript, AJAX, JSON, JQuery, Angular, Git,
-          Responsive Design, Semantic, SEO, Wordpress,HTML, CSS, SASS,
-          Javascript, AJAX, JSON, JQuery, Angular, Git, Responsive Design,
+          HTML, CSS, SASS, React, Javascript, Next.JS, Taiwind, AJAX, JSON,
+          JQuery, Git, Responsive Design, Semantic, SEO, Wordpress,HTML, CSS,
+          SASS, Javascript, AJAX, JSON, JQuery, Angular, Git, Responsive Design,
           Semantic, SEO, Wordpress
         </S.Item>
       </S.Items>
